156463717f5868ad6fbc66540a0d24acc5cc47f9
Padraig Gleeson <p.gleeson@gmail.com>
2013-09-03 14:44:36 +0100

    Update to v2beta2

--- src/main/java/org/neuroml/export/Main.java
+++ src/main/java/org/neuroml/export/Main.java
@@ -4,7 +4,7 @@ package org.neuroml.export;
 public class Main 
 {
 
-    public static String ORG_NEUROML_EXPORT_VERSION = "1.1.3";
+    public static String ORG_NEUROML_EXPORT_VERSION = "1.2.0";
 
     public static String getLemsExamplesResourcesDir()
     {
