db461875300205b27077e4202e8dda5595a22892
Boris Marin <borismarin@gmail.com>
2013-11-14 14:36:59 +0000

    truncating floats for export, better array dumping

--- src/main/java/org/neuroml/export/info/model/PlotNode.java
+++ src/main/java/org/neuroml/export/info/model/PlotNode.java
@@ -1,39 +1,10 @@
-/*******************************************************************************
- * The MIT License (MIT)
- * 
- * Copyright (c) 2011, 2013 OpenWorm.
- * http://openworm.org
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the MIT License
- * which accompanies this distribution, and is available at
- * http://opensource.org/licenses/MIT
- *
- * Contributors:
- *     	OpenWorm - http://openworm.org/people.html
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights 
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
- * copies of the Software, and to permit persons to whom the Software is 
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- *******************************************************************************/
 package org.neuroml.export.info.model;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.neuroml.export.info.model.pairs.IterablePair;
+import org.neuroml.export.info.model.pairs.Pair;
 
 /**
  * @author borismarin
@@ -95,7 +66,22 @@ public class PlotNode
 	@Override
 	public String toString()
 	{
-		return "PlotNode [Title=" + _plotTitle + ", X=" + _xAxisLabel + ", Y=" + _yAxisLabel + ", Data=" + _data + "]";
+		return "PlotNode [Title=" + _plotTitle + ", X=" + _xAxisLabel + ", Y=" + _yAxisLabel + ", Data=" + toTruncatedArray(_data) + "]";
+	}
+
+	private String toTruncatedArray(List<Data> _data) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(Data data : _data)
+		{
+			IterablePair<Double,Double> listPair = new IterablePair<Double, Double>(data.getXData(),data.getYData());
+			for (Pair<Double, Double> d: listPair)
+			{
+				sb.append(String.format("%.4f %.4f", d.first(), d.second()));
+			}
+		}
+		return sb.toString();
+
 	}
 	
 	
---
+++ src/main/java/org/neuroml/export/info/model/pairs/ParallelIterator.java
@@ -0,0 +1,27 @@
+package org.neuroml.export.info.model.pairs;
+
+import java.util.Iterator;
+
+public class ParallelIterator <T1, T2> implements Iterator<Pair<T1, T2>> {
+
+    private final Iterator<T1> it1;
+    private final Iterator<T2> it2;
+
+    public ParallelIterator(Iterator<T1> it1, Iterator<T2> it2) { 
+        this.it1 = it1; this.it2 = it2;
+    }
+
+    @Override
+    public boolean hasNext() { return it1.hasNext() && it2.hasNext(); }
+
+    @Override
+    public Pair<T1, T2> next() {
+        return new Pair<T1, T2>(it1.next(), it2.next());
+    }
+
+    @Override
+    public void remove(){
+        it1.remove();
+        it2.remove();
+    }
+}
