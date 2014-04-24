5d9742ce1b60635c19b9c6eb5dcb7eac65b2a84b
Padraig Gleeson <p.gleeson@gmail.com>
2014-04-24 11:04:27 +0100

    Initial CellMLWriter

---
+++ src/main/java/org/neuroml/export/cellml/CellMLWriter.java
@@ -0,0 +1,221 @@
+/**
+ *
+ */
+package org.neuroml.export.cellml;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import org.lemsml.export.base.GenerationException;
+
+import org.neuroml.export.Utils;
+import org.neuroml.export.base.XMLWriter;
+
+import org.lemsml.jlems.core.type.dynamics.OnCondition;
+import org.lemsml.jlems.core.type.dynamics.OnStart;
+import org.lemsml.jlems.core.type.dynamics.StateAssignment;
+import org.lemsml.jlems.core.type.dynamics.StateVariable;
+import org.lemsml.jlems.core.type.dynamics.TimeDerivative;
+import org.lemsml.jlems.core.expression.ParseError;
+import org.lemsml.jlems.core.expression.ParseTree;
+import org.lemsml.jlems.core.expression.Parser;
+import org.lemsml.jlems.core.logging.E;
+import org.lemsml.jlems.core.type.Component;
+import org.lemsml.jlems.core.type.ComponentType;
+import org.lemsml.jlems.core.type.Target;
+import org.lemsml.jlems.core.type.FinalParam;
+import org.lemsml.jlems.core.sim.ContentError;
+import org.lemsml.jlems.core.type.Constant;
+import org.lemsml.jlems.core.type.Lems;
+import org.lemsml.jlems.core.type.dynamics.DerivedVariable;
+import org.lemsml.jlems.core.type.dynamics.Dynamics;
+import org.lemsml.jlems.io.util.FileUtil;
+
+@SuppressWarnings("StringConcatenationInsideStringBufferAppend")
+public class CellMLWriter extends XMLWriter {
+
+    public static final String PREF_CELLML_SCHEMA = "cellml.xsd";
+    public static final String LOCAL_CELLML_SCHEMA = "???.xsd";
+
+    public static final String GLOBAL_TIME_CELLML = "time";
+    public static final String GLOBAL_TIME_CELLML_MATHML = "<ci>time</ci>";
+
+    //private final String sbmlTemplateFile = "sbml/template.sbml";
+    public CellMLWriter(Lems l) {
+        super(l, "CellML");
+    }
+
+    @Override
+    public String getMainScript() throws GenerationException {
+
+        Parser p = new Parser();
+
+        StringBuilder main = new StringBuilder();
+
+        try {
+            Target target = lems.getTarget();
+
+            Component simCpt = target.getComponent();
+
+            String targetId = simCpt.getStringValue("target");
+
+            Component tgtNet = lems.getComponent(targetId);
+
+            String netId = tgtNet.getID();
+
+            ArrayList<Component> pops = tgtNet.getChildrenAL("populations");
+
+            main.append("<?xml version='1.0' encoding='UTF-8'?>\n");
+            //<model name="van_der_pol_model_1928" cmeta:id="van_der_pol_model_1928" xmlns="http://www.cellml.org/cellml/1.0#" 
+            //  xmlns:cellml="http://www.cellml.org/cellml/1.0#" xmlns:cmeta="http://www.cellml.org/metadata/1.0#">
+
+            String[] attrs = new String[]{"name=" + netId,
+                "cmeta:id=" + netId,
+                "xmlns=http://www.cellml.org/cellml/1.0#",
+                "xmlns:cellml=http://www.cellml.org/cellml/1.0#",
+                "xmlns:cmeta=http://www.cellml.org/metadata/1.0#"};
+            startElement(main, "model", attrs);
+            startElement(main, "notes");
+            startElement(main, "p", "xmlns=http://www.w3.org/1999/xhtml");
+            main.append("\n" + Utils.getHeaderComment(format) + "\n");
+            main.append("\nExport of model:\n" + lems.textSummary(false, false) + "\n");
+            endElement(main, "p");
+            endElement(main, "notes");
+            main.append("\n");
+
+            startElement(main, "component", "name=environment");
+            startEndElement(main, "variable", "name=time", "units=millisecond", "public_interface=out");
+            endElement(main, "component");
+            main.append("\n");
+
+            addComment(main, "Adding simulation " + simCpt + " of network: " + tgtNet.summary() + "", true);
+
+            int initAssNum = 0;
+            int onCondNum = 0;
+
+            for (Component pop : pops) {
+                handleComponent(main, pop);
+                String compRef = pop.getStringValue("component");
+                Component popComp = lems.getComponent(compRef);
+                handleComponent(main, popComp);
+
+                /*
+                 ComponentType type = popComp.getComponentType();
+                 initAssNum = initAssNum + type.getDynamics().getOnStarts().size();
+                 onCondNum = onCondNum + type.getDynamics().getOnConditions().size();
+
+                 int num = Integer.parseInt(pop.getStringValue("size"));
+                 addComment(main, "Population " + pop.getID() + " contains " + num + " instances of components of: " + popComp, true);
+
+                 for (int i = 0; i < num; i++) {
+                 startEndElement(main, "compartment", "id=" + pop.getID() + "_" + i, "size=1");
+                 }*/
+            }
+
+            main.append("\n");
+
+            endElement(main, "model");
+
+        } catch (ContentError e) {
+            throw new GenerationException("Error with LEMS content", e);
+        }
+        return main.toString();
+    }
+
+    private void handleComponent(StringBuilder main, Component comp) throws ContentError {
+
+        startElement(main, "component", "name=" + comp.id);
+
+        //<variable name="time" units="millisecond" public_interface="in"/>
+        startEndElement(main, "variable", "name=time", "units=millisecond", "public_interface=in");
+
+        for (FinalParam param : comp.getComponentType().getFinalParams()) {
+            startEndElement(main,
+                    "variable",
+                    "name=" + param.getName(),
+                    "initial_value=" + (float) comp.getParamValue(param.getName()).getDoubleValue(),
+                    "units=dimensionless");
+        }
+
+        Dynamics dyn = comp.getComponentType().getDynamics();
+        if (dyn != null) {
+            for (StateVariable sv : dyn.getStateVariables()) {
+                startEndElement(main,
+                        "variable",
+                        "name=" + sv.getName(), 
+                        "initial_value=0",
+                        "units=dimensionless");
+
+            }
+        
+
+            startElement(main,"math", "xmlns=http://www.w3.org/1998/Math/MathML");
+            
+            for (DerivedVariable dv: dyn.getDerivedVariables()) {
+                startElement(main, "apply");
+                startElement(main, "eq");
+                startEndTextElement(main, "ci", dv.getName());
+                processMathML(main, dv.getParseTree(), false);
+                endElement(main, "eq");
+                endElement(main, "apply");
+                
+            }
+        /*
+            for (TimeDerivative td : dyn.getTimeDerivatives()) {
+                startElement(main, "rateRule", "variable=" + td.getStateVariable().getName());
+                            //MathMLWriter mmlw = new MathMLWriter();
+                //E.info("TD: "+mmlw.serialize(td.getParseTree()));
+                processMathML(main, td.getParseTree(), false);
+                endElement(main, "rateRule");
+
+            }*/
+
+            for (OnStart os : dyn.getOnStarts()) {
+                for (StateAssignment sa : os.getStateAssignments()) {
+                    startElement(main, "assignmentRule", "variable=" + sa.getStateVariable().getName());
+                    processMathML(main, sa.getParseTree(), false);
+                    endElement(main, "assignmentRule");
+
+                }
+            }
+            
+            endElement(main,"math");
+        }
+
+        endElement(main, "component");
+        main.append("\n\n");
+
+    }
+
+    private String getSuitableId(String str) {
+        return str.replace(" ", "_").replace(".", "").replace("(", "_").replace(")", "_").replace("*", "mult").replace("+", "plus").replace("/", "div");
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        E.setDebug(false);
+
+        ArrayList<File> lemsFiles = new ArrayList<File>();
+        //lemsFiles.add(new File("../NeuroML2/LEMSexamples/LEMS_NML2_Ex0_IaF.xml"));
+        //lemsFiles.add(new File("../NeuroML2/LEMSexamples/LEMS_NML2_Ex5_DetCell.xml"));
+        lemsFiles.add(new File("../NeuroML2/LEMSexamples/LEMS_NML2_Ex9_FN.xml"));
+        lemsFiles.add(new File("../git/HindmarshRose1984/NeuroML2/Run_Regular_HindmarshRose.xml"));
+        //lemsFiles.add(new File("../neuroConstruct/osb/cerebellum/cerebellar_granule_cell/GranuleCell/neuroConstruct/generatedNeuroML2/LEMS_GranuleCell.xml"));
+        //lemsFiles.add(new File("../neuroConstruct/osb/invertebrate/lobster/PyloricNetwork/neuroConstruct/generatedNeuroML2/LEMS_PyloricPacemakerNetwork.xml"));
+        //lemsFiles.add(new File("../neuroConstruct/osb/invertebrate/celegans/CElegansNeuroML/CElegans/pythonScripts/c302/LEMS_c302_A.xml"));
+        //lemsFiles.add(new File("../git/GPUShowcase/NeuroML2/LEMS_simplenet.xml"));
+
+        for (File lemsFile : lemsFiles) {
+            Lems lems = Utils.readLemsNeuroMLFile(lemsFile).getLems();
+            File mainFile = new File(lemsFile.getParentFile(), lemsFile.getName().replaceAll(".xml", ".cellml"));
+
+            CellMLWriter cw = new CellMLWriter(lems);
+            String ff = cw.getMainScript();
+            FileUtil.writeStringToFile(ff, mainFile);
+            System.out.println("Generated: " + mainFile.getAbsolutePath());
+        }
+
+    }
+
+}
