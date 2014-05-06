53d356c6eb68e19d695df7d238eaf53de86da55e
Padraig Gleeson <p.gleeson@gmail.com>
2014-05-06 17:38:21 +0100

    Improved export of explicitInputs to Neuron

--- src/main/java/org/neuroml/export/neuron/NeuronWriter.java
+++ src/main/java/org/neuroml/export/neuron/NeuronWriter.java
@@ -560,6 +560,7 @@ public class NeuronWriter extends BaseWriter {
             } else {
                 secName = popName+"["+cellNum+"]";
             }
+            inputName += "_"+popName+"_"+cellNum+"_"+secName.replaceAll("\\[", "").replaceAll("\\]", "");
 
 			addComment(main, "Adding input: " + explInput);
 
@@ -2097,7 +2098,7 @@ public class NeuronWriter extends BaseWriter {
         lemsFiles.add(new File("../NeuroML2/LEMSexamples/LEMS_NML2_Ex9_FN.xml"));
         lemsFiles.add(new File("../neuroConstruct/osb/cerebellum/cerebellar_granule_cell/GranuleCell/neuroConstruct/generatedNeuroML2/LEMS_GranuleCell.xml"));
         //lemsFiles.add(new File("../neuroConstruct/osb/invertebrate/lobster/PyloricNetwork/neuroConstruct/generatedNeuroML2/LEMS_PyloricPacemakerNetwork.xml"));
-        lemsFiles.add(new File("../neuroConstruct/osb/invertebrate/celegans/CElegansNeuroML/CElegans/pythonScripts/c302/LEMS_c302_A_Pharyngeal.xml"));
+        lemsFiles.add(new File("../neuroConstruct/osb/invertebrate/celegans/CElegansNeuroML/CElegans/pythonScripts/c302/LEMS_c302_A_Syns.xml"));
         //lemsFiles.add(new File("../git/GPUShowcase/NeuroML2/LEMS_simplenet.xml"));
         //lemsFiles.add(new File("../git/BlueBrainProjectShowcase/ChannelTest/LEMS_TestVClamp.xml"));
         
