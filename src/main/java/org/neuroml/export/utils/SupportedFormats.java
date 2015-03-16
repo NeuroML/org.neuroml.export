e7963b4261a0780705b691328812ba0b45bc9bd5
Adrian Quintana <adrian.perez@ucl.ac.uk>
2015-03-16 14:42:32 +0000

    improve exception management

--- src/main/java/org/neuroml/export/utils/SupportedFormats.java
+++ src/main/java/org/neuroml/export/utils/SupportedFormats.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.lemsml.jlems.core.logging.E;
 import org.lemsml.jlems.core.sim.LEMSException;
 import org.lemsml.jlems.core.type.Lems;
 import org.neuroml.export.exceptions.ModelFeatureSupportException;
@@ -16,29 +17,18 @@ public class SupportedFormats
 		return Arrays.asList(Format.values());
 	}
 	
-	public static List<Format> getSupportedOutputs(Lems lems){
+	public static List<Format> getSupportedOutputs(Lems lems) throws NeuroMLException, LEMSException{
 		List<Format> supportedOutputs = new ArrayList<Format>();
 		for (Format format : Format.values()){
-			
 			try
 			{
 				ExportFactory.getExportWriter(lems, format);
 				supportedOutputs.add(format);
+				E.info("Format " + format + " supported");
 			}
 			catch(ModelFeatureSupportException e)
 			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			catch(NeuroMLException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			catch(LEMSException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				E.info("Format " + format + " not supported");
 			}
 		}
 		
