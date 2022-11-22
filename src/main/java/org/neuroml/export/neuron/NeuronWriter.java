04bcb29abe8ec4214d124d5143ead1e21c7f9881
Padraig Gleeson <p.gleeson@gmail.com>
2022-11-22 14:36:03 +0000

    Support __SIMULATOR__ & __TIMESTAMP__ in report files for nrn/npyne

--- src/main/java/org/neuroml/export/neuron/NeuronWriter.java
+++ src/main/java/org/neuroml/export/neuron/NeuronWriter.java
@@ -49,6 +49,7 @@ import org.lemsml.jlems.core.type.dynamics.TimeDerivative;
 import org.lemsml.jlems.core.type.dynamics.Transition;
 import org.lemsml.jlems.core.type.simulation.EventWriter;
 import org.lemsml.jlems.io.util.FileUtil;
+import org.lemsml.jlems.io.IOUtil;
 import org.neuroml.export.base.ANeuroMLBaseWriter;
 import org.neuroml.export.exceptions.GenerationException;
 import org.neuroml.export.exceptions.ModelFeatureSupportException;
@@ -355,7 +356,8 @@ public class NeuronWriter extends ANeuroMLBaseWriter
             if (target.reportFile!=null)
             {
                 main.append(bIndent+"import socket\n");
-                main.append(bIndent+"self.report_file = open('"+target.reportFile+"','w')\n");
+                String reportFile = IOUtil.getCompleteReportFileName(target.reportFile, "NEURON", null);
+                main.append(bIndent+"self.report_file = open('"+reportFile+"','w')\n");
                 main.append(bIndent+"print('Simulator version:  %s'%h.nrnversion())\n");
                 main.append(bIndent+"self.report_file.write('# Report of running simulation with %s\\n'%h.nrnversion())\n");
                 main.append(bIndent+"self.report_file.write('Simulator=NEURON\\n')\n");
@@ -2497,7 +2499,7 @@ public class NeuronWriter extends ANeuroMLBaseWriter
             if(species == null || species.equals("non_specific"))
             {
                 blockBreakpoint.append("i = gion * (v - e)\n");
-                blockBreakpoint.append("i__" + mechName + " = -1 * i  : set this variable to the current also\n");
+                blockBreakpoint.append("i__" + mechName + " = -1 * i  : set this variable to the current also - note -1 as channel current convention for LEMS used!\n");
             }
             else
             {
@@ -2513,7 +2515,7 @@ public class NeuronWriter extends ANeuroMLBaseWriter
                 {
                     blockBreakpoint.append("i" + species + " = gion * ghk2(v, cai, cao)\n");
                 }
-                blockBreakpoint.append("i__" + mechName + " =  -1 * i" + species + " : set this variable to the current also\n");
+                blockBreakpoint.append("i__" + mechName + " =  -1 * i" + species + " : set this variable to the current also - note -1 as channel current convention for LEMS used!\n");
             }
         }
 //        else if(comp.getComponentType().isOrExtends(NeuroMLElements.BASE_SYNAPSE_COMP_TYPE))
