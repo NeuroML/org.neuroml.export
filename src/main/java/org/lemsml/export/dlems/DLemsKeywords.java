fe6009b6fd6b2b17b06977afa6a651c7a6891374
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2024-02-15 12:25:45 +0000

    feat(dlems-keywords): add cvode related keywords

--- src/main/java/org/lemsml/export/dlems/DLemsKeywords.java
+++ src/main/java/org/lemsml/export/dlems/DLemsKeywords.java
@@ -2,79 +2,83 @@ package org.lemsml.export.dlems;
 
 /**
  * @author matteocantarelli
- * 
+ *
  */
 public enum DLemsKeywords
 {
-	DT, 
-	SEED, 
-	REPORT_FILE, 
-    DYNAMICS, 
-    EVENTS, 
-    CONDITION, 
-    DIRECTION, 
-    EFFECT, 
-    NAME, 
-    TYPE, 
-    TITLE, 
-    PARAMETERS, 
-    STATE, 
-    STATE_FUNCTIONS, 
-    T_END, 
-    T_START, 
-    COMMENT, 
-    DUMP_TO_FILE, 
-    OUTPUT_FILE, 
-    SPIKE_FILE, 
-    SPIKE_FILE_FORMAT, 
-    EVENT_SELECTIONS, 
-    EVENT_SELECTION_ID, 
-    SELECT, 
-    EVENT_PORT, 
-    FILE_NAME, 
-    OUTPUT_COLUMNS, 
-    VARIABLE, 
-    QUANTITY, 
-    DISPLAY, 
-    ABSCISSA_AXIS, 
-    MIN, MAX, 
-    ORDINATE_AXIS, 
-    CURVES, 
-    ABSCISSA, 
-    ORDINATE, 
-    POPULATION, 
-    POPULATION_INDEX, 
-    SEGMENT_ID, 
-    SEGMENT_NAME, 
-    FRACTION_ALONG, 
-    COLOUR, 
-    POPULATIONS, 
-    SIZE, 
+    DT,
+    SEED,
+    REPORT_FILE,
+    DYNAMICS,
+    EVENTS,
+    CONDITION,
+    DIRECTION,
+    EFFECT,
+    NAME,
+    TYPE,
+    TITLE,
+    PARAMETERS,
+    STATE,
+    STATE_FUNCTIONS,
+    T_END,
+    T_START,
+    COMMENT,
+    DUMP_TO_FILE,
+    OUTPUT_FILE,
+    SPIKE_FILE,
+    SPIKE_FILE_FORMAT,
+    EVENT_SELECTIONS,
+    EVENT_SELECTION_ID,
+    SELECT,
+    EVENT_PORT,
+    FILE_NAME,
+    OUTPUT_COLUMNS,
+    VARIABLE,
+    QUANTITY,
+    DISPLAY,
+    ABSCISSA_AXIS,
+    MIN, MAX,
+    ORDINATE_AXIS,
+    CURVES,
+    ABSCISSA,
+    ORDINATE,
+    POPULATION,
+    POPULATION_INDEX,
+    SEGMENT_ID,
+    SEGMENT_NAME,
+    FRACTION_ALONG,
+    COLOUR,
+    POPULATIONS,
+    SIZE,
     SYNAPSES,
     SYNAPSE,
     COMPONENT,
-    PROJECTIONS, 
-    PRE_POPULATION, 
-    POST_POPULATION, 
+    PROJECTIONS,
+    PRE_POPULATION,
+    POST_POPULATION,
     CONNECTIONS,
-    PRE_CELL_ID, 
-    POST_CELL_ID, 
-    WEIGHT, 
-    DELAY, 
+    PRE_CELL_ID,
+    POST_CELL_ID,
+    WEIGHT,
+    DELAY,
     INPUTS,
-    
-    
+
+
     TEMPERATURE,
     NEURON_VARIABLE_NAME,
     NEURON_MECHANISM_NAME,
     NEURON_SECTION_NAME,
     NEURON_FRACT_ALONG,
     NEURON_VARIABLE_SCALE,
-    
-    EXPORT_LIBRARY_VERSION;
 
-	public String get()
-	{
-		return this.toString().toLowerCase();
-	}
+    EXPORT_LIBRARY_VERSION,
+
+    CVODE,
+    ABS_TOL,
+    REL_TOL;
+
+    public String get()
+    {
+        return this.toString().toLowerCase();
+    }
 }
